3b3b9660bd9e20c66eae6ce4d0670414d1b4756f
nataniel <artur@jedlinski.pl>
2019-08-23 23:29:19 +0200

    remove flaticon from flash doctrine production settings

--- application/config/environment/production.php
+++ application/config/environment/production.php
@@ -16,5 +16,15 @@ return [
     'base_url' => 'http://www.xpect.pl/kalambury/',
     'ssl_required' => false,
 
+    /**
+     * Doctrine configuration settings for Entity Manager
+     * @link http://docs.doctrine-project.org/en/2.0.x/reference/configuration.html
+     */
+    'doctrine' => [
+        'auto_generate_proxies' => false,
+        'cache_class' => \Doctrine\Common\Cache\ApcuCache::class,
+        'cache_namespace' => 'adventure',
+    ],
+
     'show_errors' => false,
 ];
