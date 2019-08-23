9c635fe54dae7c8f72acd40569c3123531039779
nataniel <artur@jedlinski.pl>
2019-08-23 23:14:38 +0200

    environment

--- application/config/environment/production.php
+++ application/config/environment/production.php
@@ -1,5 +1,14 @@
 <?php
 return [
+    'database' => [
+        'driver' => 'pdo_mysql',
+        'dbname' => 'kalambury',
+        'user' => '******',
+        'password' => '******',
+        'host' => 'localhost',
+        'charset' => 'utf8',
+    ],
+
     /**
      * Base URL for non-http requests and all other cases when
      * the Base URL cannot be determined automatically from $_SERVER
@@ -7,5 +16,5 @@ return [
     'base_url' => 'http://www.xpect.pl/kalambury/',
     'ssl_required' => false,
 
-    'show_errors' => true,
+    'show_errors' => false,
 ];
